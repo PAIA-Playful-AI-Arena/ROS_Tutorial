d66009079380f1c07ce54cc6da4329e15ef02613
Kylin_on_ASUS <kylingithubdev@gmail.com>
2022-06-13 13:14:09 +0800

    add launch

--- scripts/build_ament_cmake_msg.bash
+++ scripts/build_ament_cmake_msg.bash
@@ -1,6 +1,7 @@
 export PKG_NAME=my_custom_ament_cmake_interface
+rosdep install --from-paths src -y --ignore-src
 # export NODE_NAME=py_talker
 # !!!! should be at workspace root !!!!
-colcon build --symlink-install --packages-select  $PKG_NAME
+colcon build --symlink-install --packages-select  $PKG_NAME 
 . install/setup.bash
 # ros2 run $PKG_NAME $NODE_NAME
