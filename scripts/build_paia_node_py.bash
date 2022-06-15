a17f476b87bd99b22ea05885bc0aa4ae17a1c2dc
ivanchiou <wyattkidd@gmail.com>
2022-06-15 10:10:01 +0800

    added build requirements script

--- scripts/build_paia_node_py.bash
+++ scripts/build_paia_node_py.bash
@@ -1,4 +1,5 @@
 export PKG_NAME=paia_node_py 
 # !!!! should be at workspace root !!!!
+pip install -r src/$PKG_NAME/requirements.txt
 colcon build --packages-select paia_demo_interface --symlink-install --event-handlers console_direct+
 colcon build --packages-select $PKG_NAME --symlink-install --event-handlers console_direct+
