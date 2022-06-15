82e8b86074cc16cc14b1dfebc6b6aba9df6e9e66
ivanchiou <wyattkidd@gmail.com>
2022-06-15 09:38:44 +0800

    added paia_demo_interface build

--- scripts/build_paia_node_py.bash
+++ scripts/build_paia_node_py.bash
@@ -1,4 +1,4 @@
 export PKG_NAME=paia_node_py 
 # !!!! should be at workspace root !!!!
 colcon build --packages-select $PKG_NAME --symlink-install --event-handlers console_direct+
-
+colcon build --packages-select paia_demo_interface --symlink-install --event-handlers console_direct+
