43dae011f30350f0e7c0198f5f2fc9b6c8e5ee84
Kylin_on_ASUS <kylingithubdev@gmail.com>
2022-06-17 16:06:34 +0800

    update

--- scripts/run_paia_node_publish.bash
+++ scripts/run_paia_node_publish.bash
@@ -1,4 +1,4 @@
-export PKG_NAME=paia_node_py 
-export EXECUTABLE=publish
+# export PKG_NAME=paia_node_py 
+# export EXECUTABLE=publish
 . install/setup.bash 
-ros2 run $PKG_NAME $EXECUTABLE 
+ros2 run paia_node_py publish
--- src/paia_node_py/paia_node_py/config.py
+++ src/paia_node_py/paia_node_py/config.py
@@ -18,3 +18,7 @@ class ServiceSettings(BaseSettings):
 publisher_setting = PublisherSettings(_env_file = path.join(path.dirname(__file__),"..","config",'publisher_and_subscriber.env'))
 subscriber_setting = SubscriberSettings(_env_file = path.join(path.dirname(__file__),"..","config",'publisher_and_subscriber.env'))
 service_setting = ServiceSettings(_env_file = path.join(path.dirname(__file__),"..","config",'service.env'))
+
+# publisher_setting = PublisherSettings()
+# subscriber_setting = SubscriberSettings()
+# service_setting = ServiceSettings()
